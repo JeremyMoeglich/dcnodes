16337ae08711166f28eb57db863936b66f6a2742
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-02-21 06:57:03 +0100

    start implementation

---
+++ src/lib/renderer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { SvelteComponentTyped } from 'svelte';
+	import { omit } from 'lodash-es';
+	type items_type = Array<{
+		component: new (...args: unknown[]) => SvelteComponentTyped<Omit<items_type, 'component'>>;
+		props: Record<string, unknown>;
+	}>;
+	export let items: items_type;
+</script>
+
+{#each items as item}
+	<svelte:component this={item.component} {...item.props} />
+{/each}
