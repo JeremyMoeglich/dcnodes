7ba2eb1ae87f6c5fd1a73154a01b2d3bd4e9d2e0
MP Play <jeremy.moeglich@gmail.com>
2022-04-05 02:07:20 +0200

    added connector remove partial

--- src/lib/in_connector.svelte
+++ src/lib/in_connector.svelte
@@ -10,10 +10,15 @@
 	} from './types/item';
 
 	export let data: data_refrence;
+	export let allow_multiple_connections = false;
 	export let value: unknown;
 	export let name: string;
 	export let direction: vector;
 	export let on_value_change: () => void = noop;
+	let start_connectors = new Set<connector_identifier<'start'>>();
+	let dragged_start_connector: connector_identifier<'start'> | undefined;
+	let last_drag_position: vector | undefined;
+
 	let element: HTMLElement | undefined;
 
 	function get_relative_to_renderer(position: vector): vector {
@@ -29,6 +34,38 @@
 		return get_relative_to_renderer(rect);
 	}
 
+	function remove_previous_connection() {
+		if (last_drag_position) {
+			data.get_current_item_refrence().remove_node_connection(name, last_drag_position);
+		}
+	}
+
+	function take_connection(event: DragEvent) {
+		if (start_connectors.size === 0) {
+			return;
+		}
+		dragged_start_connector = start_connectors.values().next()
+			.value as connector_identifier<'start'>;
+		start_connectors.delete(dragged_start_connector);
+		data
+			.get_items()
+			[dragged_start_connector.id].get_node_connections()
+			[dragged_start_connector.name].delete(self_data);
+		if (event.dataTransfer) {
+			event.dataTransfer.setData('text/plain', JSON.stringify(dragged_start_connector));
+		}
+		during_drag(event);
+	}
+
+	function during_drag(event: DragEvent) {
+		remove_previous_connection();
+		if (dragged_start_connector === undefined) {
+			return;
+		}
+		const drag_position = get_relative_to_renderer({ x: event.pageX, y: event.pageY });
+		data.get_current_item_refrence().add_node_connection(name, drag_position);
+		last_drag_position = drag_position;
+	}
 	function drop(event: DragEvent) {
 		const drop_data: connector_identifier<'start'> = JSON.parse(
 			event.dataTransfer?.getData('text/plain') ?? '{ node_id: -1, name: "invalid" }'
@@ -38,7 +75,14 @@
 			return;
 		}
 		const drop_item_refrence: item_type_refrence = data.get_items()[drop_data.id];
-		drop_item_refrence.add_node_connection(drop_data.name, self_data);
+		if (allow_multiple_connections) {
+			drop_item_refrence.add_node_connection(drop_data.name, self_data);
+			start_connectors.add(drop_data);
+			start_connectors = start_connectors;
+		} else {
+			drop_item_refrence.set_node_connections({ [drop_data.name]: new Set([self_data]) });
+			start_connectors = new Set([drop_data]);
+		}
 	}
 
 	data.set_connector(name, {
@@ -60,7 +104,18 @@
 		on:dragover={(event) => {
 			event.preventDefault();
 		}}
+		on:dragstart={(event) => {
+			take_connection(event);
+		}}
+		on:drag={(event) => {
+			during_drag(event);
+		}}
+		on:dragend={(event) => {
+			remove_previous_connection();
+			dragged_start_connector = undefined;
+		}}
 		on:drop={drop}
+		draggable={start_connectors.size > 0}
 		bind:this={element}
 	>
 		<div class="main" />
